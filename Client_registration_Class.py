2c0396d8d68e9c6cd2108b54f444bf71f26d3a81
josiasjp <josiasdesenvolvedor@gmail.com>
2022-11-30 14:04:54 -0300

    ajuste da função do CPF e variaveis do telefone

--- Client_registration_Class.py
+++ Client_registration_Class.py
@@ -3,22 +3,26 @@ from random import randrange
 import os
 
 class Client():
-    def __init(self, id, Name, Cpf, Birth_Date_day, Birth_Date_month, Birth_Date_year, Gender, Road, Number, CEP, District, State):
+    def __init(self, id, Name, Cpf, Birth_Date_day, Birth_Date_month, Birth_Date_year, Gender, Road, Number, CEP, District, State, phone_country_code, phone_ddd, phone_number):
         self.client_id = id
         self.client_name = str(Name.upper())
         self.client_birth_date = date(Birth_Date_year, Birth_Date_month,Birth_Date_day)
         self.client_gender = Gender
-        self.client_address = {Road,Number, CEP, District, State}
+        self.client_address = {"Hroad":Road,"Hnumber":Number, "Hcep":CEP, "Hdistrict":District, "Hstate":State}
+        self.client_phone_num = {"country_code": phone_country_code, "ddd": phone_ddd, "num":phone_number}
     
     def TipeVerify():
         return 0
     
     def CpfAutentication(self):
-        if len(self.client_cpf) < 11:
+        if len(self.client_cpf) == 11:
             self.client_cpf = self.client_cpf.zfill(11)
             self.client_cpf = '{}.{}.{}-{}'.format(self.client_cpf[:3], self.client_cpf[3:6], self.client_cpf[6:9], self.client_cpf[9:])
             return True
-        else:
+        elif len(self.client_cpf) >= 10 or len(self.client_cpf) <= 12:
+            print("this number is not a cpf")
+            return False
+        else:   
             self.client_cpf = '000.000.000-00'
             return False
     
